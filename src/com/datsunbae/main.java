e935ee463d195fa7816203d123e4523172441fd1
Nguyen Van Dat <datsunbae@gmail.com>
2023-03-03 10:38:32 +0700

    Solve conflict Rebase: NguyenVanDat_1911062150

--- src/com/datsunbae/main.java
+++ src/com/datsunbae/main.java
@@ -9,9 +9,12 @@ public class main {
 		System.out.println("Edit file on branch master");
 		System.out.println("Edit file on branch NguyenVanDat");
 		System.out.println("Edit branch NguyenVanDat");
-
-
 		System.out.println("Rebase Conflict: branch on master");
+		System.out.println("Rebase conflict: on branch NguyenVanDat");
+		System.out.println("Rebase conflict: on branch NguyenVanDat");
+		System.out.println("Rebase conflict: on branch NguyenVanDat");
+		System.out.println("Rebase conflict: on branch NguyenVanDat");
+		System.out.println("Rebase conflict: on branch NguyenVanDat");
 
 	}
 

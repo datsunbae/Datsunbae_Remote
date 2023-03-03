f2602fac281a5228589a0fe32eeb368ce7d72403
Nguyen Van Dat <datsunbae@gmail.com>
2023-03-03 10:25:08 +0700

    Merget Conflict: Edit on master

--- src/com/datsunbae/main.java
+++ src/com/datsunbae/main.java
@@ -6,7 +6,7 @@ public class main {
 		System.out.println("Nguyen Van Dat - 1911062150");
 		System.out.println("Edit Clone");
 		System.out.println("Edit gitS6");
-		System.out.println("Edit file on branch NguyenVanDat");
+		System.out.println("Edit file on branch master");
 	}
 
 }
